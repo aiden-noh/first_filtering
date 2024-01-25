26547672d9b39f3c149ab390f08ef08424b965d0
노영빈 <yb.noh@phase3.team>
2024-01-25 17:44:27 +0900

    구글빅쿼리로 연간/분기 크롤링 데이터 업로드 기능 완료

--- gbq_service.py
+++ gbq_service.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import numpy as np
 import pandas as pd
 from google.cloud import bigquery
 
 client = bigquery.Client()
 
+# 연간
 def _convert(crawring_df: pd.DataFrame,
              stock_code: str,
              corp_name: str,
@@ -21,13 +23,47 @@ def _convert(crawring_df: pd.DataFrame,
     df['type'] = type
     df['updated'] = datetime.now()
 
+    # 연, 분기 데이터 세팅
+    df.columns = ['order', 'account_name', 'date', 'amount', 'stock_code', 'corp_name', 'type', 'updated']
+    df['year'] = df.date.str.split('/').str[0].astype(int)
+
     # 컬럼 정리
-    df.columns = ['order', 'account_name', 'year', 'amount', 'stock_code', 'corp_name', 'type', 'updated']
     df = df[['stock_code', 'corp_name', 'year', 'type', 'order', 'account_name', 'amount', 'updated']]
 
-    # year 변환 및 id 생성
-    df.year = df.year.str.split('/').str[0].astype('int')
+    # id 생성
     df.index = df.stock_code + '_' + df.year.astype(str) + '_' + df.type + '_' + df.order.astype(str).str.zfill(3)
+    df.index.name = 'id'
+
+    return df
+
+# 분기
+def _convert_q(crawring_df: pd.DataFrame,
+               stock_code: str,
+               corp_name: str,
+               type: str) -> pd.DataFrame:
+
+    df = crawring_df.copy()
+
+    # 순서 저장 후 짧은 구조 변환
+    df['order'] = df.index.to_list()
+    df = df.melt(['order', 'account_nm'])
+
+    # 추가 데이터 세팅
+    df['stock_code'] = stock_code
+    df['corp_name'] = corp_name
+    df['type'] = type
+    df['updated'] = datetime.now()
+
+    # 연, 분기 데이터 세팅
+    df.columns = ['order', 'account_name', 'date', 'amount', 'stock_code', 'corp_name', 'type', 'updated']
+    df['year'] = df.date.str.split('/').str[0].astype(int)
+    df['quarter'] = np.ceil(df.date.str.split('/').str[1].astype(int) / 3).astype(int)
+
+    # 컬럼 정리
+    df = df[['stock_code', 'corp_name', 'year', 'quarter', 'type', 'order', 'account_name', 'amount', 'updated']]
+
+    # id 생성
+    df.index = df.stock_code + '_' + df.year.astype(str) + '_' + df.quarter.astype(str) + '_' + df.type + '_' + df.order.astype(str).str.zfill(3)
     df.index.name = 'id'
 
     return df
@@ -56,6 +92,7 @@ def _upload(df: pd.DataFrame, table_name: str):
     job = client.load_table_from_dataframe(df, table_id)  # Make an API request.
     print(job.result())  # Wait for the job to complete.
 
+# 연간
 def upload_crawring_to_gbq(crawring_df: pd.DataFrame,
                            stock_code: str,
                            corp_name: str,
@@ -64,3 +101,13 @@ def upload_crawring_to_gbq(crawring_df: pd.DataFrame,
     df = _convert(crawring_df, stock_code, corp_name, type)
     _clean_exist(df)
     _upload(df, 'test_income_statement_year')
+
+# 분기
+def upload_crawring_to_gbq_q(crawring_df: pd.DataFrame,
+                             stock_code: str,
+                             corp_name: str,
+                             type: str):
+
+    df = _convert_q(crawring_df, stock_code, corp_name, type)
+    _clean_exist(df)
+    _upload(df, 'test_income_statement_quarter')
